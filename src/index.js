e447b389690e8ceb7dd3bca5984e5deb7646c118
dcts <thomas@liist.com>
2021-10-25 03:32:27 +0200

    :sparkle: add OpenseaScraper.offers()

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const basicInfo = require("./functions/basicInfo.js");
 const floorPrice = require("./functions/floorPrice.js");
 const floorPriceByUrl = require("./functions/floorPriceByUrl.js");
 const rankings = require("./functions/rankings.js");
+const offers = require("./functions/offers.js");
 
 const OpenseaScraper = {
   basicInfo,
   floorPrice,
   floorPriceByUrl,
   rankings,
+  offers,
 };
 
 module.exports = OpenseaScraper;
