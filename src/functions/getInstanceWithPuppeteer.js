1a044b38a2b40cb7dc4e50bb0b54d1b43c4c878c
robertistok <robertistok94@gmail.com>
2021-10-26 13:02:00 +0100

    Provide a wrapper that returns an instance with browser attached to fns

---
+++ src/functions/getInstanceWithPuppeteer.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer-extra');
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+const basicInfo = require("./basicInfo.js");
+const floorPrice = require("./floorPrice.js");
+const floorPriceByUrl = require("./floorPriceByUrl.js");
+const rankings = require("./rankings.js");
+const offers = require("./offers.js");
+
+async function OpenseaScraperInstance({ mode } = {}) {
+  const browser = await puppeteer.launch({
+    headless: mode === "debug" ? false : true,
+    args: ['--start-maximized'],
+  });
+
+  return {
+    close: () => browser.close(),
+    basicInfo: (...params) => basicInfo(...params), // no browser here, as it's an api call
+    floorPrice: (...params) => floorPrice(...params, { ...(params && params.opts ? params.opts : {}), browser}),
+    floorPriceByUrl: (...params) => floorPriceByUrl(...params, { ...(params && params.opts ? params.opts : {}), browser}),
+    rankings: (...params) => rankings(...params, { ...(params && params.opts ? params.opts : {}), browser}),
+    offers: (...params) => offers(...params, { ...(params && params.opts ? params.opts : {}), browser}),
+  }
+}
+
+module.exports = OpenseaScraperInstance
