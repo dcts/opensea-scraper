a65639b96ce9dd72ded8f7c91bdb64ff619a1e14
SyraTi <syra-ti@do-core.cn>
2021-10-26 17:14:36 +0800

    fix: wrong typescript return type definition in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module 'opensea-scraper' {
 
-  export function floorPrice(slug: string): number|undefined;
-  export function basicInfo(slug: string): Record<string, any>;
+  export function floorPrice(slug: string): Promise<number|undefined>;
+  export function basicInfo(slug: string): Promise<Record<string, any>>;
 
 }
