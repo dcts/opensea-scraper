550c97403f925b865de4c90661cda4b9fccc202e
robertistok <robertistok94@gmail.com>
2021-10-27 10:47:08 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,30 @@
-declare module 'opensea-scraper' {
+interface IRanking {
+  slug: string;
+  thumbanil?: string;
+  rank: number;
+}
 
-  export function floorPrice(slug: string): number|undefined;
-  export function basicInfo(slug: string): Record<string, any>;
+interface IOffer {
+  floorPrice: { amount: number; currency: string };
+  tokenId: number;
+  tokenName: string;
+}
 
+declare module "opensea-scraper" {
+  export function basicInfo(slug: string): Promise<Record<string, any>>;
+  export function floorPrice(
+    slug: string,
+    mode?: string
+  ): Promise<number | undefined>;
+  export function floorPriceByUrl(
+    slug: string,
+    mode?: string
+  ): Promise<number | undefined>;
+  export function rankings(nPages?: string, mode?: string): Promise<IRanking[]>;
+  export function offers(nPages?: string, mode?: string): Promise<IOffer[]>;
+  export function offersByUrl(
+    url: string,
+    resultSize?: number,
+    mode?: string
+  ): Promise<IOffer[]>;
 }
