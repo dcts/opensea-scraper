4f2b71d401fc4291faa4815648913c4028258a90
dcts <thomas@liist.com>
2021-10-28 00:46:24 +0200

    :fire: remove redundancy for offers() function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const basicInfo = require("./functions/basicInfo.js");
 const floorPrice = require("./functions/floorPrice.js");
 const floorPriceByUrl = require("./functions/floorPriceByUrl.js");
 const rankings = require("./functions/rankings.js");
-const offers = require("./functions/offers.js");
-const offersByUrl = require("./functions/offersByUrl.js");
+const { offers, offersByUrl } = require("./functions/offers.js");
 
 
 const OpenseaScraper = {
