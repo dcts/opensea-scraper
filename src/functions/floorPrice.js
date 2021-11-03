ca168bcbf0d92debd59dc09b414717f3a851134c
dcts <thomas@liist.com>
2021-11-03 18:33:40 +0100

    :recycle: add new method for floor prices

--- src/functions/floorPrice.js
+++ src/functions/floorPrice.js
@@ -1,3 +1,4 @@
+const { offersByUrl } = require("./offers.js");
 // puppeteer-extra is a drop-in replacement for puppeteer,
 // it augments the installed puppeteer with plugin functionality
 const puppeteer = require('puppeteer-extra');
@@ -6,63 +7,23 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 /**
- * Scrapes the actual floor price from the opensea website
- * mode is either "headless" (default) or "debug"
- * => run in debug mode to show browser interaction (no headless mode)
- *    and avoid closing browser when the function ends
+ * Gets the floor price of a given collection from opensea.
+ * uses `OpenseaScraper.offers()` under the hood.
+ * `slug` is the collection identifier by opensea
  */
-const floorPrice = async (slug, mode = "headless") => {
+const floorPrice = async (slug) => {
   const url = `https://opensea.io/collection/${slug}?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW`;
-  return await floorPriceByUrl(url, mode);
+  return await floorPriceByUrl(url);
 }
 
 /**
- * use custom url to scrape floorPrice
+ * use custom url to scrape the floor price
+ * Opensea supports encoding filtering in the URL so this method is helpful for getting
+ * a specific asset (for example floor price for a LAND token from the sandbox collection)
  */
-const floorPriceByUrl = async (url, mode = "headless") => {
-  const browser = await puppeteer.launch({
-    headless: mode === "debug" ? false : true,
-    args: ['--start-maximized'],
-  });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  // ...🚧 waiting for cloudflare to resolve
-  await page.waitForSelector('.cf-browser-verification', {hidden: true});
-
-  const floorPrice = await page.evaluate(() => {
-    const cardsNodeList = document.querySelectorAll(".Asset--anchor .AssetCardFooter--price-amount");
-    const cardsArray = Array.prototype.slice.call(cardsNodeList); // you cannot use .map on a nodeList, we need to transform it to an array
-    const floorPrices = cardsArray.map(card => {
-      try {
-        // only fetch price in ETH
-        if (!card.querySelector(".Price--eth-icon")) {
-          return undefined;
-        }
-        const priceStr = card.querySelector(".Price--amount").textContent;
-        return Number(priceStr.split(",").join("."));
-      } catch(err) {
-        return undefined;
-      }
-    }).filter(val => val); // filter out invalid (undefined) values
-    // if no ETH price is found, return undefined
-    if (floorPrices.length === 0) {
-      return undefined;
-    }
-    // sometimes the order of elements is not accurate on Opensea,
-    // thats why we need to minimize get the lowest value
-    // REMARK: do not remove spread operator, see explenation here: https://dev.to/thebronxsystem/math-min-array-needs-spread-operator-1oe7
-    const floorPrice = Math.min(...floorPrices);
-    return {
-      amount: floorPrice,
-      currency: "ETH",
-    }
-  });
-
-  if (mode !== "debug") {
-    await browser.close();
-  }
-  return floorPrice;
+const floorPriceByUrl = async (url) => {
+  const result = await offersByUrl(url, {sort: true});
+  return result.offers[0].floorPrice;
 }
 
 module.exports = {
