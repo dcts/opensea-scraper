89123f0dd41077a3ec4527cadce5ad52a5e80356
dcts <thomas@liist.com>
2021-11-03 19:56:05 +0100

    :sparkle: update demo to v5

--- demo.js
+++ demo.js
@@ -12,25 +12,12 @@ console.log(`===>>> ${slug} <<<===`);
 console.log("OPTIONS:");
 console.log(options);
 
-
 (async () => {
   // basic info
   console.log(`\n\n\n\n✅ === OpenseaScraper.basicInfo(slug) ===`);
   const basicInfo = await OpenseaScraper.basicInfo(slug);
   console.log(`basic info (taken from the opensea API):`);
   console.log(basicInfo);
-
-  // get the current floor price
-  console.log(`\n\n\n\n✅ === OpenseaScraper.floorPrice(slug) ===`);
-  const floorPrice = await OpenseaScraper.floorPrice(slug);
-  console.log(`...done! 🔥 \nfloor price:`);
-  console.dir(floorPrice, {depth: null});
-
-  // get floor price by url
-  console.log(`\n\n\n\n✅ === OpenseaScraper.floorPriceByUrl(url) ===`);
-  console.log("scraping floor price from custom url... (to fetch floor price for a SANDBOX LAND token)")
-  const floorPriceByUrl = await OpenseaScraper.floorPriceByUrl("https://opensea.io/collection/sandbox?search[sortAscending]=true&search[sortBy]=PRICE&search[stringTraits][0][name]=Type&search[stringTraits][0][values][0]=Land&search[toggles][0]=BUY_NOW");
-  console.dir(floorPriceByUrl, {depth: null});
 
   // get offers
   console.log(`\n\n\n\n✅ === OpenseaScraper.offers(slug) ===`);
@@ -49,8 +36,9 @@ console.log(options);
 
   // scrape rankings => https://opensea.io/rankings?sortBy=total_volume
   console.log(`\n\n\n\n✅ === OpenseaScraper.rankings() ===`);
-  console.log("scraping 1 page of rankings => https://opensea.io/rankings?sortBy=total_volume");
-  const rankings = await OpenseaScraper.rankings(options);
+  const nbrOfPages = 2;
+  console.log(`scraping ${nbrOfPages} pages of rankings => https://opensea.io/rankings?sortBy=total_volume`);
+  const rankings = await OpenseaScraper.rankings(nbrOfPages, options);
   console.log(`scraped ${rankings.length} collections: ${rankings.map(o => o.slug).join(" | ")}`);
 
   console.log("\n🎉 DEMO ENDED 🥳")
