30c411c10bed675bae90466be0ddbf7b4341e518
dcts <thomas@liist.com>
2021-11-03 19:56:36 +0100

    :fire: depricate floorPrice() function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const basicInfo = require("./functions/basicInfo.js");
-const { floorPrice, floorPriceByUrl } = require("./functions/floorPrice.js");
 const { offers, offersByUrl } = require("./functions/offers.js");
 const rankings = require("./functions/rankings.js");
 
 const OpenseaScraper = {
   basicInfo,
-  floorPrice,
-  floorPriceByUrl,
   offers,
   offersByUrl,
   rankings,
