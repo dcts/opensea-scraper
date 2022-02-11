77a87a6d1ad7cbc491945dbd19b3fea25c3e6cff
dcts <thomas@liist.com>
2022-02-11 21:00:01 +0100

    :sparkle: scrape from HTML, only top 100

--- src/functions/rankings.js
+++ src/functions/rankings.js
@@ -9,7 +9,7 @@ puppeteer.use(StealthPlugin());
 const { warnIfNotUsingStealth } = require("../helpers/helperFunctions.js");
 
 /**
- * Scrapes all collections from the Rankings page at https://opensea.io/rankings?sortBy=total_volume
+ * Scrapes all collections from the Rankings page at https://opensea.io/rankings
  * options = {
  *   nbrOfPages: number of pages that should be scraped? (defaults to 1 Page = top 100 collections)
  *   debug: [true,false] enable debugging by launching chrome locally (omit headless mode)
@@ -17,7 +17,7 @@ const { warnIfNotUsingStealth } = require("../helpers/helperFunctions.js");
  *   browserInstance: browser instance created with puppeteer.launch() (bring your own puppeteer instance)
  * }
  */
-const rankings = async (nbrOfPages, optionsGiven = {}) => {
+const rankings = async (type = "total", optionsGiven = {}) => {
   const optionsDefault = {
     debug: false,
     logs: false,
@@ -26,7 +26,7 @@ const rankings = async (nbrOfPages, optionsGiven = {}) => {
   const options = { ...optionsDefault, ...optionsGiven };
   const { debug, logs, browserInstance } = options;
   const customPuppeteerProvided = Boolean(optionsGiven.browserInstance);
-  logs && console.log(`=== OpenseaScraper.rankings() ===\n...fetching ${nbrOfPages} pages (= top ${nbrOfPages*100} collections)`);
+  logs && console.log(`=== OpenseaScraper.rankings() ===\n`);
 
   // init browser
   let browser = browserInstance;
@@ -39,60 +39,65 @@ const rankings = async (nbrOfPages, optionsGiven = {}) => {
   customPuppeteerProvided && warnIfNotUsingStealth(browser);
 
   const page = await browser.newPage();
-  const url = "https://opensea.io/rankings?sortBy=total_volume";
+  const url = getUrl(type);
   logs && console.log("...opening url: " + url);
   await page.goto(url);
 
   logs && console.log("...🚧 waiting for cloudflare to resolve");
   await page.waitForSelector('.cf-browser-verification', {hidden: true});
 
-  logs && console.log("...exposing helper functions through script tag")
-  await page.addScriptTag({path: require.resolve("../helpers/rankingsHelperFunctions.js")});
+  logs && console.log("extracting __NEXT_DATA variable");
+  const __NEXT_DATA__ = await page.evaluate(() => {
+    const nextDataStr = document.getElementById("__NEXT_DATA__").innerText;
+    return JSON.parse(nextDataStr);
+  });
 
-  logs && console.log("...scrolling to bottom and fetching collections.");
-  let dict = await _scrollToBottomAndFetchCollections(page);
-
-  // scrape n pages
-  for (let i = 0; i < nbrOfPages - 1; i++) {
-    await _clickNextPageButton(page);
-    await page.waitForSelector('.Image--image');
-    logs && console.log("...scrolling to bottom and fetching collections. Items fetched so far: " + Object.keys(dict).length);
-    dict = await _scrollToBottomAndFetchCollections(page);
-  }
-  await browser.close();
-  // transform dict to array + remove invalid results
-  const filtered = Object.values(dict).filter(o => o.rank !== 0 && o.name !== "");
-  logs && console.log("...🥳 DONE. Total Collections fetched: " + Object.keys(dict).length);
-  // order by rank
-  return filtered.sort((a,b) => a.rank - b.rank);
+  // extract relevant info
+  const top100 = _parseNextDataVarible(__NEXT_DATA__);
+  logs && console.log(`🥳 DONE. Total ${top100.length} Collections fetched: `);
+  return top100;
 }
 
+function _parseNextDataVarible(__NEXT_DATA__) {
+  const extractFloorPrice = (statsV2) => {
+    try {
+      return {
+        amount: Number(statsV2.floorPrice.eth),
+        currency: "ETH",
+      }
+    } catch(err) {
+      return null;
+    }
+  }
+  const extractCollection = (obj) => {
+    return {
+      name: obj.name,
+      slug: obj.slug,
+      logo: obj.logo,
+      isVerified: obj.isVerified,
+      floorPrice: extractFloorPrice(obj.statsV2),
+      // statsV2: obj.statsV2, // 🚧 comment back in if you need additional stats
+    };
+  }
+  return __NEXT_DATA__.props.relayCache[0][1].json.data.rankings.edges.map(obj => extractCollection(obj.node));
+}
+
+function getUrl(type) {
+  if (type === "24h") {
+    return "https://opensea.io/rankings?sortBy=one_day_volume";
+
+  } else if (type === "7d") {
+    return "https://opensea.io/rankings?sortBy=seven_day_volume";
+
+  } else if (type === "30d") {
+    return "https://opensea.io/rankings?sortBy=thirty_day_volume";
+
+  } else if (type === "total") {
+    return "https://opensea.io/rankings?sortBy=total_volume";
+
+  } else {
+    throw new Error(`Invalid type provided. Expected: 24h,7d,30d,total. Got: ${type}`);
+  }
+}
 module.exports = rankings;
 
-
-/**
- * Helper Functions for OpenseaScraper.rankings()
- */
-async function _clickNextPageButton(page) {
-  await page.click('[value=arrow_forward_ios]');
-}
-async function _scrollToBottomAndFetchCollections(page) {
-  return await page.evaluate(() => new Promise((resolve) => {
-    // keep in mind inside the browser context we have the global variable "dict" initialized
-    // defined inside src/helpers/rankingsHelperFunctions.js
-    var scrollTop = -1;
-    const interval = setInterval(() => {
-      console.log("another scrol... dict.length = " + Object.keys(dict).length);
-      window.scrollBy(0, 50);
-      // fetchCollections is a function that is exposed through page.addScript() and
-      // is defined inside src/helpers/rankingsHelperFunctions.js
-      fetchCollections(dict);
-      if(document.documentElement.scrollTop !== scrollTop) {
-        scrollTop = document.documentElement.scrollTop;
-        return;
-      }
-      clearInterval(interval);
-      resolve(dict);
-    }, 5);
-  }));
-}
