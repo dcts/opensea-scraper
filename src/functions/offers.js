f55ea8ba0062ded92609d9405cb77e0222a946a2
dcts <thomas@liist.com>
2022-07-07 12:42:36 +0200

    :sparkle: add additionalWait option

--- src/functions/offers.js
+++ src/functions/offers.js
@@ -6,7 +6,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 // load helper function to detect stealth plugin
-const { warnIfNotUsingStealth } = require("../helpers/helperFunctions.js");
+const { warnIfNotUsingStealth, sleep } = require("../helpers/helperFunctions.js");
 
 /**
  * scrapes opensea offers for a given collection.
@@ -47,10 +47,11 @@ const offersByUrl = async (url, optionsGiven = {}) => {
     debug: false,
     logs: false,
     sort: true, // sorts the returned offers by lowest to highest price
+    additionalWait: 0, // waittime in milliseconds, after page loaded, but before stating to scrape
     browserInstance: undefined,
   };
   const options = { ...optionsDefault, ...optionsGiven };
-  const { debug, logs, browserInstance, sort } = options;
+  const { debug, logs, browserInstance, sort, additionalWait } = options;
   const customPuppeteerProvided = Boolean(optionsGiven.browserInstance);
 
   // add mandatory query params
@@ -83,6 +84,12 @@ const offersByUrl = async (url, optionsGiven = {}) => {
   // ...🚧 waiting for cloudflare to resolve
   logs && console.log("🚧 waiting for cloudflare to resolve...");
   await page.waitForSelector('.cf-browser-verification', {hidden: true});
+
+  // additional wait?
+  if (additionalWait > 0) {
+    logs && console.log(`additional wait active, waiting ${additionalWait / 1000} seconds...`);
+    await sleep(additionalWait);
+  }
 
   // extract __wired__ variable
   logs && console.log("extracting __wired__ variable");
